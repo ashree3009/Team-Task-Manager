a1399b71a2146920fb84c44d9f361dd50994cd91
ashree3009 <168769706+ashree3009@users.noreply.github.com>
2026-05-20 22:31:53 +0530

    Fix deployed API URL

--- team-task-manager/frontend/src/api/axios.js
+++ team-task-manager/frontend/src/api/axios.js
@@ -4,7 +4,7 @@ import axios from 'react';
 import axiosDefault from 'axios';
 
 const api = axiosDefault.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'import.meta.env.VITE_API_URL',
 });
 
 api.interceptors.response.use(
